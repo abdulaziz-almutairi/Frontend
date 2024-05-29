0f43e52c984cefd9dd3e8745f01fce6615e45a2b
Abdulaziz-Almutairi <aziz1418@outlook.sa>
2024-05-29 11:17:10 +0300

    Fixed Auto play in hero video

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button"
+import video from "@/assets/videos/1.mp4"
 
 export function Hero() {
   return (
@@ -6,13 +7,14 @@ export function Hero() {
       <section className="bg-gray-100 py-12 md:py-24">
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div className="position:relative; w-100%; h-100%; padding-bottom:56.410%">
-            <iframe
+            {/* <iframe
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               // src="https://v1.pinimg.com/videos/mc/720p/9c/e6/91/9ce69111f4d96bebf5eb8a3dec560d79.mp4?autoplay=1&muted=1"
               src="https://streamable.com/e/lwj0u4?autoplay=1"
               className="w-full aspect-video"
               allowFullScreen
-            ></iframe>
+            ></iframe> */}
+            <video src={video} loop autoPlay muted className="w-750 h-500" controls></video>
           </div>
           <div className="space-y-4">
             <h1 className="text-4xl font-bold">Discover the Latest Electronics</h1>
