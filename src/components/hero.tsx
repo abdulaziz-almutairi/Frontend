736c8b16c902c5ee06c0866273784e1e686a4d0b
Abdulaziz-Almutairi <aziz1418@outlook.sa>
2024-05-29 10:40:49 +0300

    Fixed Hero Section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,8 +7,9 @@ export function Hero() {
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div className="position:relative; w-100%; h-100%; padding-bottom:56.410%">
             <iframe
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              src="https://streamable.com/e/69e5hr?autoplay=1"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              // src="https://v1.pinimg.com/videos/mc/720p/9c/e6/91/9ce69111f4d96bebf5eb8a3dec560d79.mp4?autoplay=1&muted=1"
+              src="https://streamable.com/e/lwj0u4?autoplay=1"
               className="w-full aspect-video"
               allowFullScreen
             ></iframe>
